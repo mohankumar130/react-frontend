161ab0a5ceb11efb64a94e00edd586088825dc2b
Mohan <mohankumar130@gmail.com>
2025-06-30 12:38:43 +0000

    updated dashboard page

---
+++ src/pages/DevOpsPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./css/DevOpsDashboard.css";
+
+function DevOpsPage() {
+  return (
+    <div className="devops-dashboard">
+      <h1>🔧 DevOps Dashboard</h1>
+      <p>Welcome to the DevOps panel. 🚀 CI/CD, Containers, Monitoring and more coming soon!</p>
+    </div>
+  );
+}
+
+export default DevOpsPage;
